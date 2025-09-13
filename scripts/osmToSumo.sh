98faebbf3922e58f13f8e63818436a0d30e20844
Arham Aneeq <arhamaneeq18@gmail.com>
2025-09-13 23:44:32 +0530

    Routes are auto generated from OSM now

--- scripts/osmToSumo.sh
+++ scripts/osmToSumo.sh
@@ -1,18 +1,28 @@
-# !/bin/bash
-
+#!/bin/bash
 set -e
 
-if [ $# -ne 2 ]; then
-  echo "Usage: $0 <input.osm> <output.net.xml>"
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <input.osm>"
   exit 1
 fi
 
-INPUT_OSM=$1
-OUTPUT_NET=$2
+NAME=$1
 
-netconvert --osm-files "$INPUT_OSM" \
-           --output-file "$OUTPUT_NET" \
+# Convert OSM to SUMO network
+netconvert --osm-files "data/maps/$NAME.osm" \
+           --output-file "data/nets/$NAME.net.xml" \
            --tls.guess --tls.join \
            --geometry.remove \
            --roundabouts.guess \
            --ramps.guess
+
+# Generate random trips
+python3 $SUMO_HOME/tools/randomTrips.py \
+        -n "data/nets/$NAME.net.xml" \
+        -o "data/trips/$NAME.trips.xml" \
+        -p 5
+
+# Generate routes
+duarouter --net-file "data/nets/$NAME.net.xml" \
+          --route-files "data/trips/$NAME.trips.xml" \
+          --output-file "data/routes/$NAME.rou.xml"
